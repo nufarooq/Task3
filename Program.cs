93da95b3eb482f9f8b87bde17c67fbd8114ddfd9
Tejora <numan.992@gmail.com>
2023-01-20 19:46:27 +0530

    Task 3 commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace VehicleInterfaceTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(0);
+
+            Console.WriteLine("Enter Gasoline Amount: ");
+            int fuel = int.Parse(Console.ReadLine());
+            if (car.Refuel(fuel))
+            {
+                car.Drive();
+            }
+        }
+    }
+
+    interface IVehiculo
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+
+    public class Car : IVehiculo
+    {
+        public int Gasoline { get; set; }
+
+        public Car(int gasoline)
+        {
+            Gasoline = gasoline;
+        }
+        public void Drive()
+        {
+            if (Gasoline > 0)
+            {
+                Console.WriteLine("The Car is Driving");
+            }
+            else
+            {
+                Console.WriteLine("No Gas");
+            }
+        }
+
+        public bool Refuel(int amount)
+        {
+            Gasoline += amount;
+            return true;
+        }
+    }
+}
